488ef56a2da27118f5fe4c31d077200191865a49
Anthony Will Solsol Soplin <asolsol@infocasas.com.uy>
2023-11-03 14:20:39 -0500

    asd

--- src/screens/Ingreso/components/Pesaje.tsx
+++ src/screens/Ingreso/components/Pesaje.tsx
@@ -3,19 +3,30 @@ import { Button, Text } from "react-native-paper"
 import TaraSection from "./Pesaje/TaraSection";
 import SimpleCard from "./Pesaje/SimpleCard";
 import AddPesajeSection from "./Pesaje/AddPesajeSection";
+import React from "react";
 
 const Pesaje = ({ navigation } : any) => {
+    const [items, setItems] = React.useState([
+        {id: 1, peso: 98, fecha: '2021-09-01'},
+        {id: 2, peso: 98, fecha: '2021-09-01'},
+        {id: 3, peso: 98, fecha: '2021-09-01'},
+    ])
+    const saveData = (data: any) => {
+        console.log('saveData', data)
+        setItems([...items, {id: items.length + 1, peso: data, fecha: '2021-09-01'}])
+    }
     return (<>
-    <View style={{ padding: 10 }}>
-        <TaraSection />
-        <Text style={{ marginVertical: 10, fontWeight: 'bold', color: 'grey' }}>Lista de pesaje</Text>
+        <View style={{ padding: 10 }}>
+            <TaraSection />
+            <Text style={{ marginVertical: 10, fontWeight: 'bold', color: 'grey' }}>Lista de pesaje</Text>
+            {
+                items.map((item) => (
+                    <SimpleCard key={item.id} />
+                ))
+            }
 
-        <SimpleCard />
-        <SimpleCard />
-        <SimpleCard />
-
-    </View>
-    <AddPesajeSection />
+        </View>
+        <AddPesajeSection onSaved={saveData} />
     </>)
 }
 
--- src/screens/Ingreso/components/Pesaje/AddPesajeSection.tsx
+++ src/screens/Ingreso/components/Pesaje/AddPesajeSection.tsx
@@ -1,20 +1,27 @@
 import React from "react";
-import {Dimensions} from "react-native";
+import {Dimensions, Image, View} from "react-native";
 import { Button, Modal, Portal, Text } from "react-native-paper";
+import BalanceImg from '../../../../../assets/img/balance.png';
 
 const deviceHeight = Dimensions.get('window').height
-const deviceWidth = Dimensions.get('window').width
 
-const AddPesajeSection = () => {
+
+const AddPesajeSection = ({onSaved = (data: number) => {}}) => {
     const [visible, setVisible] = React.useState(false);
     const showModal = () => setVisible(true);
     const hideModal = () => setVisible(false);
     const containerStyle = {
-        height: deviceHeight - (deviceHeight * 0.5),
+        height: deviceHeight - (deviceHeight * 0.3),
         padding: 20,
         margin: 20,
         backgroundColor: 'white',
     };
+
+    const save = () => {
+        console.log('save')
+        onSaved(1)
+        hideModal()
+    }
 
     return (
         <>
@@ -25,7 +32,17 @@ const AddPesajeSection = () => {
             </Button>
             <Portal>
                 <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={containerStyle}>
-                    <Text> {deviceHeight} Example Modal.  Click outside this area to dismiss.</Text>
+                    <View style={{ alignItems: 'center' }}>
+                        <Text style={{ color: 'grey', fontSize: 30, marginTop: 50, fontWeight: 'bold', textAlign: 'center' }}> 98 Kg</Text>
+                        <Image source={BalanceImg} style={{ width: 150, height: 90, marginHorizontal: 'auto' }} resizeMode="contain" />
+                        <Text style={{ color: '#d1d1d1' }}> Datos obtenidos de la balanza eléctrica</Text>
+                        <Button style={{ marginTop: 50, width: '100%' }} mode="outlined" onPress={save}>
+                            Registrar manualmente
+                        </Button>
+                        <Button style={{ marginTop: 20,  width: '100%' }} mode="contained" onPress={save}>
+                            Guardar
+                        </Button>
+                    </View>
                 </Modal>
             </Portal>
         </>
