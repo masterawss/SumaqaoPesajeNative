8a90b1141f528a3d3a7d45d4dbea3c872a875fac
Anthony Will Solsol Soplin <asolsol@infocasas.com.uy>
2023-11-10 15:03:57 -0500

    as

--- src/screens/Ingreso/Show/Header.tsx
+++ src/screens/Ingreso/Show/Header.tsx
@@ -2,14 +2,48 @@ import { useNavigation } from "@react-navigation/native";
 import { Appbar } from "react-native-paper"
 import { TicketContext } from "./provider/TicketProvider";
 import { useContext } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import api from "../../../utils/axios";
+import Snackbar from "react-native-snackbar";
 
 const Header = () => {
     const navigation = useNavigation();
     const { loading, hasError, loadTicket, ticketPesaje, deleteTicket } = useContext(TicketContext);
 
+
+    const saveTicket = async () => {
+        const user = await AsyncStorage.getItem('user');
+        api.post('/ticket_pesaje/update/'+ticketPesaje.id, { 
+            is_saved: 1,
+            updated_user_id: JSON.parse(user || "")?.id
+        })
+        .then((response) => {
+            console.log(response.data)
+            loadTicket()
+        })
+        .catch((error) => {
+            console.log(error.response)
+            Snackbar.show({
+                text: 'No se pudo registrar la tara',
+                duration: Snackbar.LENGTH_INDEFINITE,
+                action: {
+                  text: 'Cerrar',
+                  textColor: 'red',
+                  onPress: () => { /* Do something. */ },
+                },
+            });
+        })
+        .finally(() => {
+            // setLoadingTara(false)
+        })
+    }
+
     return <Appbar.Header>
         <Appbar.BackAction onPress={() => {navigation.goBack()}} />
         <Appbar.Content title="Ticket de pesaje" />
+        {
+            !!ticketPesaje.is_saved && <Appbar.Action icon="save" onPress={saveTicket} />
+        }
         <Appbar.Action icon="pencil" onPress={() => {}} />
         <Appbar.Action icon="delete" onPress={deleteTicket} />
         <Appbar.Action icon="sync" onPress={loadTicket} />
